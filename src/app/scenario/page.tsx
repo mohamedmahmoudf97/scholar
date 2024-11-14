c6defa622eb096053749389adca7c2d26b60a631
Mohamed Fouad <mohamed.mahmoud.f.97@gmail.com>
2024-11-14 13:30:12 +0200

    Update Styling

--- src/app/scenario/page.tsx
+++ src/app/scenario/page.tsx
@@ -2,7 +2,7 @@ import ScenarioView from '@/features/scenarios/views';
 
 export default function ScenarioPage() {
   return (
-    <div className="flex flex-col items-center min-h-screen p-8  bg-gray-100">
+    <div className="flex flex-col items-center min-h-screen p-4 sm:p-8 bg-gray-100">
       <ScenarioView />
     </div>
   );
